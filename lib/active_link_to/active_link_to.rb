8cc0bc86537340f06a9ea749e21af04323a1c63e
xdite <xuite.joke@gmail.com>
2014-01-23 15:37:40 +0800

    split wrap_tag_css

--- lib/active_link_to/active_link_to.rb
+++ lib/active_link_to/active_link_to.rb
@@ -24,7 +24,7 @@ module ActiveLinkTo
     active_options  = { }
     link_options    = { }
     html_options.each do |k, v|
-      if [:active, :class_active, :class_inactive, :active_disable, :wrap_tag].member?(k)
+      if [:active, :class_active, :class_inactive, :active_disable, :wrap_tag, :wrap_tag_css].member?(k)
         active_options[k] = v
       else
         link_options[k] = v
@@ -35,16 +35,19 @@ module ActiveLinkTo
     css_class << active_link_to_class(url, active_options)
     css_class.strip!
 
-    wrap_tag = active_options[:wrap_tag].present? ? active_options[:wrap_tag] : nil
-    link_options[:class] = css_class if css_class.present?
+    wrap_tag = active_options[:wrap_tag]
+    wrap_tag_css = active_options[:wrap_tag_css]
 
-    link = if active_options[:active_disable] === true && is_active_link?(url, active_options[:active])
+    link_options[:class] = css_class
+
+    link = if active_options[:active_disable].present? && is_active_link?(url, active_options[:active])
       content_tag(:span, name, link_options)
     else
       link_to(name, url, link_options)
     end
 
-    wrap_tag ? content_tag(wrap_tag, link, :class => (css_class if css_class.present?)) : link
+    wrap_tag ? content_tag(wrap_tag, link, :class =>  wrap_tag_css ) : link
+ 
   end
 
   # Returns css class name. Takes the link's URL and its params
